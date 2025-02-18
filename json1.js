cd0d97fd0ba7adc950c0c8204888bc581af058f2
Kalp Soneji <kalpsonejicod@gmail.com>
2025-02-18 20:03:55 +0530

    json intro

---
+++ json1.js
@@ -0,0 +1,33 @@
+let whatsapp = {
+    chats: {
+        chat1: {
+            fname: "Vatsal",
+            lname: "Shah",
+            number: [1234567890, 9876543210]
+        },
+        chat2: {
+            fname: "Harshita",
+            lname: "Sobhani",
+            number: 9876543210
+        }
+    },
+    statuses: {
+        status1: {
+            fname: "Abhijeet",
+            lname: "Shah",
+            number: 1234567890
+        },
+        status2: {
+            fname: "Rohan",
+            lname: "Shah",
+            number: 9876543210
+        },
+    },
+    Communities: {
+        chatname1: "Royal Hackathon",
+        chatname2: "Royal Avengers",
+        chatname3: "Royal Internship"
+    }
+}
+
+console.log(whatsapp.chats.chat1.number[0]);
