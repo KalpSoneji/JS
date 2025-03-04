ffa684184403dea08b1962309c8dfcb95b492484
Kalp Soneji <kalpsonejicod@gmail.com>
2025-03-04 20:18:06 +0530

    stringrev

--- stringrev.js
+++ stringrev.js
@@ -1,5 +1,10 @@
-let username = prompt("Enter name: ");
+var string = "ijenoS plaK";
 
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
 
+var reverse = reverseString(string);
 
-console.log(x);
+console.log(reverse);
+
