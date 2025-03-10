a5fc01d8c957642a57b83ac296e1b079185c3d85
Kalp Soneji <kalpsonejicod@gmail.com>
2025-03-10 23:53:28 +0530

    .

--- Array/array2.js
+++ Array/array2.js
@@ -2,17 +2,28 @@ var arr = [1, 5, 10, 20, 102];
 
 console.log(arr);
 
-console.log("Length of array: ", arr.length);
+// console.log("Length of array: ", arr.length);
 
-console.log("Pushing 201 into array at end: ", arr.push(201), arr);
+// console.log("Popping an element from array from end : ", arr.pop(), arr); //only 1 element can be popped at a time
 
-console.log("Popping 201 from array from end : ", arr.pop(), arr);
+// console.log("Pushing an element into array at end: ", arr.push(201), arr); //multiple elements can be pushed at a time
 
-console.log("Adding 333 at starting of the array: ", arr.unshift(333), arr);
+// console.log("Adding element at starting of the array: ", arr.unshift(333), arr); //multiple elements can be pushed at a time
 
-console.log("Removing 333 from starting of the array: ", arr.shift(), arr);
+// console.log("Removing element from starting of the array: ", arr.shift(), arr); //only 1 element can be popped at a time
 
-console.log("Sorting array: ", arr.sort(), arr);
+// console.log("Sorting array: ", arr.sort()); //sorts array in asc order
 
-console.log("Converting array into a string: ", arr.join(), arr);
+// console.log("Converting array into a string: ", arr.join()); 
 
+// console.log("Reversing, the array: ", arr.reverse());
+
+// console.log("We can find the index of a particular element using: ", arr.indexOf(10)); 
+
+// console.log("Using includes, we can check if 5 is present in the array or not: ", arr.includes(5));
+
+// console.log("In order to access some elements from between, use slice: ", arr.slice(1, 4)); // excludes the last index
+
+// console.log("In order to add or remove elements from between the array, use splice: ", arr.splice(2, 2, "EOL"), arr);
+
+//We can add or remove any number of elements from between
